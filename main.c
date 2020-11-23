7753cd07f669949c97ca4eff6c9d09b6b965be0f
dev-Hamza-3 <74596914+dev-Hamza-3@users.noreply.github.com>
2020-11-23 16:48:58 +0100

    Positions generator

--- main.c
+++ main.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 int main()
 {
-    printf("Hello world!\n");
-    return 0;
+    char i;
+    int j;
+    while(1)
+    {
+    srand(time(NULL));
+    i = 65 + rand()%8;
+    srand(time(NULL));
+    j = rand()%8+1;
+    printf("%c%d \n",i,j);
+    sleep(1);
+    }
 }
